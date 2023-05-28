7108bea686984859b82a0833c3b43e6d0f329a5d
S0ulx3 <98002038+S0ulx3@users.noreply.github.com>
2023-05-28 04:02:13 +0200

    Update Calculator.sh
    Actualización de Colores
    
    Colors Update

--- Calculator.sh
+++ Calculator.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
+# ANSI
+
+RED='\033[31m'
+GREEN='\033[32m'
+BLUE='\033[34m'
+YELLOW='\033[33m'
+NC='\033[0m' # Restablecer color
+
+
 # Solicitar al usuario el primer numero
 echo
-read -p "Ingresa el primer numero: " num1
+read -p "$(echo -e "${BLUE}Ingresa el primer numero:${NC} ")" num1
 # Solicitar al usuario el segundo numero
 echo
-read -p "Ingresa el segundo numero: " num2
+read -p "$(echo -e "${BLUE}Ingresa el segundo numero:${NC} ")" num2
 
 # Mostrar las opciones disponibles
 echo
-echo "Seleccionar la operacion:"
-echo "1. Suma"
-echo "2. Resta"
-echo "3. Multiplicacion"
-echo "4. Division"
+echo -e "${RED}Seleccionar la operacion:"
+echo -e "1. Suma"
+echo -e "2. Resta"
+echo -e "3. Multiplicacion"
+echo -e "4. Division${NC}"
 
 # Solicitar al usuario que elija una opcion
 echo
-read -p "Ingresa el numero de la operacion que desea realizar:" opcion
+read -p "$(echo -e "${BLUE}Ingresa el numero de la operacion que desea realizar:${NC} ")" opcion
 
 # Realizar la operación seleccionada
 case $opcion in
@@ -33,11 +42,13 @@ case $opcion in
   4) resultado=$(echo "scale=2; $num1 / $num2" | bc)
      operacion="división"
      ;;
-  *) echo "Opción inválida"
+  *)
+     echo
+     echo -e "${RED}Opción inválida${NC}"
      exit 1
      ;;
 esac
 
 # Mostrar el resultado
 echo
-echo "El resultado de la $operacion es: $resultado"
+echo -e "${BLUE}El resultado de la $operacion es:${NC} ${GREEN}$resultado${NC}"
